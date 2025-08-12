670f40547280e752b6298e5573001baaa7a59de8
juanbuitrago505 <jbuitrago505@gmail.com>
2025-08-12 16:10:23 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,11 @@
+<?php
+$identificacion = $_POST['identificacion'];
+$NomApellidos = $_POST['NomApellidos'];
+$Cargo = $_POST['cargo'];
+$Salario = $_POST['salario'];
 
+echo "La identificacion del Empleado es: ".$identificacion;
+echo "Los nombres y apellidos del empleado son: ".$NomApellidos;
+echo "El cargo del empleado es: ".$Cargo;
+echo "El salario del empleado es: ".$Salario;
+?>
